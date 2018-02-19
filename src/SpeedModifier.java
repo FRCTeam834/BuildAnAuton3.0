943556512ef9d8c41b6a47533aaf7ba5d55f3b6b
CorvinoW <34324401+CorvinoW@users.noreply.github.com>
2018-02-19 16:33:10 -0500

    Fixed Speed Editing

--- src/SpeedModifier.java
+++ src/SpeedModifier.java
@@ -1,35 +1,15 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 
 public class SpeedModifier extends JFrame{
-
-	//Theoretically highlights the line
-	SpeedModifier() {
-		setVisible(false);
-		JTextField indexSetter = new JTextField(6);
-		JLabel indexLabel = new JLabel ("Line");
-		JTextField speedSetter = new JTextField(6);
-		JLabel speedLabel = new JLabel ("Speed (-0.5 to 0.5)");
-		JButton setSpeed = new JButton("Set Speed");
-		setSize(150,175);
-		setLayout(new FlowLayout());
-		add(indexLabel);
-		add(indexSetter);
-		add(speedLabel);
-		add(speedSetter);
-		add(setSpeed);
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		getRootPane().setDefaultButton(setSpeed);
-		setSpeed.addActionListener((ActionEvent e) -> {
-			int indexSet = (int) Double.parseDouble(indexSetter.getText());
-			double speedSet = Double.parseDouble(speedSetter.getText());
-			BuildAnAuton.speeds.set(indexSet, speedSet);
-			indexSetter.setText("");
-			speedSetter.setText("");
-		});	
+	//Changes the Speed over the line
+	public static void speedTool(){
+		int tempIndex = BuildAnAuton.lineIndex;
+		String input = JOptionPane.showInputDialog(null, "Turn Speed (-0.5 to 0.5): ", BuildAnAuton.speeds.get(BuildAnAuton.lineIndex));
+		if(input == null || input == "") return;
+		double val = Double.parseDouble(input);
+		if(val < -.5) val = -.5;
+		else if(val > 0.5) val = .5;
+		BuildAnAuton.speeds.set(tempIndex, val);
 	}
 }
