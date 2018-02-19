be3d956395b55602241cd08b0735e8eda2b27014
CorvinoW <34324401+CorvinoW@users.noreply.github.com>
2018-02-19 16:36:01 -0500

    Removed a word

--- src/SpeedModifier.java
+++ src/SpeedModifier.java
@@ -5,7 +5,7 @@ public class SpeedModifier extends JFrame{
 	//Changes the Speed over the line
 	public static void speedTool(){
 		int tempIndex = BuildAnAuton.lineIndex;
-		String input = JOptionPane.showInputDialog(null, "Turn Speed (-0.5 to 0.5): ", BuildAnAuton.speeds.get(BuildAnAuton.lineIndex));
+		String input = JOptionPane.showInputDialog(null, "Speed (-0.5 to 0.5): ", BuildAnAuton.speeds.get(BuildAnAuton.lineIndex));
 		if(input == null || input == "") return;
 		double val = Double.parseDouble(input);
 		if(val < -.5) val = -.5;
